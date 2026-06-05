9b91b96c6e2458c5024cb55aca899cf7ad53f679
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-05 18:40:31 +0200

    Create taskApi.js
    Vytvoření samostatné componenty pro crud funkce. Obsahuje getTasks, getTaskDetail, createTask, assignTask, updateTaskStatus, deleteTask

---
+++ AvilokTaskAssignment-client/src/api/taskApi.js
@@ -0,0 +1,19 @@
+import { apiGet, apiPost, apiPatch, apiDelete } from "../utils/api";
+
+export const getTasks = () => 
+    apiGet("tasks");
+
+export const getTaskDetail = (taskId) =>
+    apiGet(`tasks/${taskId}`);
+
+export const createTask = (task) =>
+    apiPost("tasks", task);
+
+export const assignTask = (taskId) =>
+    apiPatch(`tasks/${taskId}/assign`, {});
+
+export const updateTaskStatus = (taskId, status) =>
+    apiPatch(`tasks/${taskId}/status`, {status});
+
+export const deleteTask = (taskId) =>
+    apiDelete(`tasks/${taskId}`);
