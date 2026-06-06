3349330bc160433cafe3494b9ead6477181546f8
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-06 14:38:49 +0200

    Update AppRouter.jsx
    Dodělány route pro task stránky.

--- AvilokTaskAssignment-client/src/router/AppRouter.jsx
+++ AvilokTaskAssignment-client/src/router/AppRouter.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { TasksPage } from "../pages/TasksPage";
+import TasksPage  from "../pages/TasksPage";
+import LoginPage from "../pages/LoginPage";
+import TaskDetailPage from "../pages/TaskDetailPage";
+import CreateTaskPage from "../pages/CreateTaskPage";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<TasksPage />} />
+
+                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/tasks/create" element={<CreateTaskPage />} />
+
+                <Route path="/task/:taskId" element={<TaskDetailPage />} />
             </Routes>
         </BrowserRouter>
     );
