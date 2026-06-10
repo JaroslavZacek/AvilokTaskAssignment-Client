33e9e42f0f87e38f36040cd958e839c862b31a1b
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-10 14:48:35 +0200

    Update LoginPage.jsx
    Vytvoření formuláře pro přihlášení

--- AvilokTaskAssignment-client/src/pages/LoginPage.jsx
+++ AvilokTaskAssignment-client/src/pages/LoginPage.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { login } from "../api/authApi";
+
 export default function LoginPage() {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        await login(email, password);
+
+        navigate("/");
+    }
+
     return (
-        <h1>Přihlášení</h1>
-    );
+        <form onSubmit={handleSubmit}>
+            <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email" 
+            />
+
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Heslo"
+            />
+            <button type="submit">
+                Přihlásit
+            </button>
+        </form>
+    )
 }
