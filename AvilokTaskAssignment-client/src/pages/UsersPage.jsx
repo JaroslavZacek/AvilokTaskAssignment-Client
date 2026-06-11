aef46981a5bd78ca2c1300956715a9c520c90010
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-11 22:54:08 +0200

    Update UsersPage.jsx
    Přidáno tlačítko Nový uživatel

--- AvilokTaskAssignment-client/src/pages/UsersPage.jsx
+++ AvilokTaskAssignment-client/src/pages/UsersPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { getUsers } from "../api/userApi";
 
@@ -20,6 +21,18 @@ export default function UsersPage() {
     return (
         <div className="container mt-4">
             <div className="d-flax justify-content-between align-items-center mb-4">
+
+                <Link
+                    to="/users/register"
+                    className="btn btn-success mb-2"
+                >
+                    <i className="bi bi-plus-lg"></i>
+                    {" "}
+                    Nový uživatel
+                </Link>
+
+                <hr />
+
                 <h2>Uživatelé</h2>
 
                 <span className="badge bg-secondary">
