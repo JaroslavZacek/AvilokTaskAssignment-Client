a25f01ee3ae1c98c0708d3387ff7cc8b090433c6
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-12 16:00:19 +0200

    Update userApi.js
    Vytvořena const removeRole

--- AvilokTaskAssignment-client/src/api/userApi.js
+++ AvilokTaskAssignment-client/src/api/userApi.js
@@ -14,5 +14,7 @@ export const getUser = async (userId) =>
     await apiGet(`users/${userId}`);
 
 export const assignRole = async (userId, roleName) =>
-    await apiPost(`users/${userId}/roles`, {roleName});
+    await apiPost(`users/${userId}/assign-role`, {roleName});
 
+export const removeRole = async (userId, roleName) =>
+    await apiDelete(`users/${userId}/remove-role`, {roleName});
