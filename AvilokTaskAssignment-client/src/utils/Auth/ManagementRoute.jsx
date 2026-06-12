fb704a342b3d2ed387ee2e47ac418c4c77f5f352
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-12 22:39:31 +0200

    Update ManagementRoute.jsx
    const isManagement je nahrazena funkcí isManagement ze souboru isManagement.js

--- AvilokTaskAssignment-client/src/utils/Auth/ManagementRoute.jsx
+++ AvilokTaskAssignment-client/src/utils/Auth/ManagementRoute.jsx
@@ -1,16 +1,12 @@
 import { Navigate } from "react-router-dom";
+
 import { useAuth } from "../../components/auth/AuthContext";
+import { isManagement } from "./isManagement";
 
 export default function ManagementRoute({ children }) {
     const { user } = useAuth();
 
-    const isManagement =
-        user?.roles?.includes("Admin") ||
-        user?.roles?.includes("Leader Developer") ||
-        user?.roles?.includes("Leader Graphic") ||
-        user?.roles?.includes("Leader Story");
-
-    return isManagement
+    return isManagement(user)
         ? children
         : <Navigate to="/" replace />
 }
