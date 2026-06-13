52e44439b0c4687f0ab36ec34c1d0d28afc669d6
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-14 00:29:47 +0200

    Update CommentCard.jsx
    Přidáno tlačítko pro smazání komentáře do těla komentáře.

--- AvilokTaskAssignment-client/src/components/comments/CommentCard.jsx
+++ AvilokTaskAssignment-client/src/components/comments/CommentCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { getComments, createComment } from "../../api/commentApi";
+import { getComments, createComment, deleteComment } from "../../api/commentApi";
 
 export default function TaskComments({ taskId, isTaskLeader }) {
     
@@ -41,6 +41,18 @@ export default function TaskComments({ taskId, isTaskLeader }) {
         }
     }
 
+    async function handleDeleteComment(commentId) {
+
+        try {
+            await deleteComment(commentId);
+
+            setComments(comments.filter(c => c.id !== commentId));
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div className="card-footer">
             <h5 className="mb-3">Komentáře</h5>
@@ -54,18 +66,41 @@ export default function TaskComments({ taskId, isTaskLeader }) {
                 )
                 : (
                     comments.map(comment => (
-                        <div key={comment.id} className="border rounded p-2 mb-2">
-                            <div className="fw-bold">
-                                {comment.authorName}
-                            </div>
+                        <div key={comment.id} className="border border-4 rounded p-2 mb-2">
+                            <div className="d-flex justify-content-between align-items-start borde">
+                                <div>
+                                    <div className="fw-bold">
+                                        {comment.authorName}
+                                    </div>
 
-                            <div className="text-muted small mb-2">
+                                    <div className="text-muted small">
+                                        {
+                                            new Date(comment.createdAt).toLocaleString("cs-CZ")
+                                        }
+                                    </div>
+                                </div>
+
                                 {
-                                    new Date(comment.createdAt).toLocaleDateString("cs-CZ")
+                                    isTaskLeader &&
+                                    (
+                                        <button
+                                            className="btn btn-outline-danger btn-sm"
+                                            onClick={() => {
+                                                if (window.confirm(
+                                                    "Opravdu chcete komentář smazat?"
+                                                )) {
+                                                    handleDeleteComment(comment.id)
+                                                }
+                                            }}                                              
+                                        >
+                                            <i className="bi bi-trash"></i>
+                                        </button>
+                                    )
                                 }
                             </div>
 
-                            <div>
+                            <hr />
+                            <div className="mt-2">
                                 {comment.text}
                             </div>
                         </div>
